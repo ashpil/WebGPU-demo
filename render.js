c6dd7c549fea28937683ab4bbe6fced5f6e23d41
ashpil <ashpil@pm.me>
2022-04-05 13:22:21 -0400

    materials

--- render.js
+++ render.js
@@ -1,20 +1,71 @@
 import "/gl-matrix-min.js";
 
+const FLOAT_SIZE = 4; // bytes
+const VEC3_SIZE = FLOAT_SIZE * 3;
+const MAT4_SIZE = FLOAT_SIZE * 16;
+
+async function main() {
+    // create initial context
+    let ctx;
+    try {
+        ctx = await new Context();
+    } catch(e) {
+        alert("WebGPU is not supported/enabled in your browser.")
+        throw e;
+    }
+
+    // load our shader module from file
+    // WGSL lets fragment and vertex live in one file
+    const shaderModule = await createShaderModule(ctx, "/shader.wgsl");
+
+    // WebGPU requires you specify the layout of data before uploading it,
+    // including binding information and what stages it is accessed in
+    const vertFragLayout = ctx.device.createBindGroupLayout({ entries: [{
+        binding: 0,
+        visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+    }]});
+    const vertLayout = ctx.device.createBindGroupLayout({ entries: [{
+        binding: 0,
+        visibility: GPUShaderStage.VERTEX,
+    }]});
+    const fragLayout = ctx.device.createBindGroupLayout({ entries: [{
+        binding: 0,
+        visibility: GPUShaderStage.FRAGMENT,
+    }]});
+
+    // our pipeline also has a fixed layout
+    const pipelineLayout = ctx.device.createPipelineLayout({ bindGroupLayouts: [vertFragLayout, vertLayout, fragLayout, fragLayout] });
+    const pipeline = createPipeline(ctx, shaderModule, pipelineLayout);
+
+    // just wait until file uploaded, then proceed
+    const inputElement = document.getElementById("file-input");
+    inputElement.addEventListener("change", async function() {
+        if (this.files.length > 0) {
+            let scene = await new Scene(ctx, vertFragLayout, vertLayout, fragLayout, this.files[0]);
+            requestAnimationFrame(() => render(ctx, pipeline, scene));
+        }
+    }, false);
+}
+
+// general context we need everywhere
 class Context {
     constructor() {
+        // theoretically having async constructors is "bad" in JS but this is easier
         return (async () => {
             if (navigator.gpu === undefined) {
                 throw "Render Error: WebGPU unsupported";
             }
 
+            // first need to get initial handles to gpu
             this.adapter = await navigator.gpu.requestAdapter();
             this.device = await this.adapter.requestDevice();
 
+            // get canvas and configure it
             const canvas = document.getElementById("webgpu-canvas");
             canvas.width = canvas.width * 4; // scale it up a bit so image looks more crisp
             canvas.height = canvas.height * 4;
             this.surface = canvas.getContext("webgpu");
-
+            this.aspect = canvas.width / canvas.height;
             this.surfaceFormat = this.surface.getPreferredFormat(this.adapter);
             this.surface.configure({
                 device: this.device,
@@ -22,6 +73,7 @@ class Context {
                 usage: GPUTextureUsage.RENDER_ATTACHMENT
             });
 
+            // need explicit depth texture for depth buffer
             this.depthFormat = "depth32float";
             this.depthTexture = this.device.createTexture({
                 size: {
@@ -38,6 +90,7 @@ class Context {
     }
 }
 
+// glb data parser
 class GlbData {
     constructor(file) {
         return (async () => {
@@ -71,22 +124,32 @@ class GlbData {
     }
 }
 
+// scene; contains meshes and camera
 class Scene {
-    constructor(ctx, meshLayout, glbFile) {
+    constructor(ctx, vertFragLayout, vertLayout, fragLayout, glbFile) {
         return (async () => {
             const glbData = await new GlbData(glbFile);
             console.log(glbData);
 
+            // load all of our materials
+            this.materials = new Array();
+            for (const material of glbData.json.materials) {
+                const color = new Float32Array(material.pbrMetallicRoughness.baseColorFactor);
+                const roughness = new Float32Array([material.pbrMetallicRoughness.roughnessFactor]);
+                this.materials.push(new Material(ctx, fragLayout, color, roughness));
+            }
+
             this.meshes = new Array();
 
-            // we need to iterate over the nodes to get camera and lights (don't need to but simplest this way)
-            // iterate through top-level nodes
+            // iterate through top-level nodes and put them into recursion stack
             const stack = new Array();
             for (const node_index of glbData.json.scenes[0].nodes) {
                 const mat = glMatrix.mat4.create();
                 stack.push({ mat, node_index });
             }
 
+            // iterate through all nodes, adding meshes with transforms as we find them
+            // we don't store the heirarchy atm as this is easier to set up
             while (stack.length) {
                 const current = stack.pop();
                 const current_node = glbData.json.nodes[current.node_index];
@@ -112,21 +175,41 @@ class Scene {
                 }
 
                 if ("camera" in current_node) {
-                    this.camera = new Camera(ctx, transformLayout, glbData.json.cameras[current_node.camera].perspective, mat); // TODO: does mat need to be inverted?
+                    this.camera = new Camera(ctx, vertFragLayout, glbData.json.cameras[current_node.camera].perspective, mat);
+                }
+
+                if ("extensions" in current_node) {
+                    if ("KHR_lights_punctual" in current_node.extensions) {
+                        // only use one point light for now
+                        if (current_node.name.startsWith("Point")) {
+                            const lightIndex = current_node.extensions.KHR_lights_punctual.light;
+                            const light = glbData.json.extensions.KHR_lights_punctual.lights[lightIndex];
+                            const position = glMatrix.vec3.create();
+                            glMatrix.vec3.transformMat4(position, position, mat);
+                            this.light = new Light(ctx, fragLayout, position, light.intensity);
+                        }
+                    }
                 }
 
                 if ("mesh" in current_node) {
                     const mesh = glbData.json.meshes[current_node.mesh];
                     const primitive = mesh.primitives[0]; // assume just one primitive
 
+                    // we keep normal and position buffers seperate as this is simpler atm
+
                     const positionIndex = primitive.attributes["POSITION"];
                     const positionView = glbData.json.bufferViews[positionIndex];
                     const positionBuffer = await glbData.binary.slice(positionView.byteOffset, positionView.byteOffset + positionView.byteLength).arrayBuffer();
 
+                    const normalIndex = primitive.attributes["NORMAL"];
+                    const normalView = glbData.json.bufferViews[normalIndex];
+                    const normalBuffer = await glbData.binary.slice(normalView.byteOffset, normalView.byteOffset + normalView.byteLength).arrayBuffer();
+
                     const indicesIndex = primitive.indices;
                     const indicesView = glbData.json.bufferViews[indicesIndex];
                     const indicesBuffer = await glbData.binary.slice(indicesView.byteOffset, indicesView.byteOffset + indicesView.byteLength).arrayBuffer();
-                    this.meshes.push(new Mesh(ctx, meshLayout, positionBuffer, indicesBuffer, mat));
+
+                    this.meshes.push(new Mesh(ctx, vertLayout, positionBuffer, normalBuffer, indicesBuffer, primitive.material, mat));
                 }
             }
 
@@ -135,16 +218,34 @@ class Scene {
     }
 }
 
+// mesh contains vertex buffer, index buffer and transform
+//
+// we have each mesh "own" a GPU buffer with a transform, as webgpu needs data uploaded ahead of time to render, which means we can't just loop over the meshes, setting the transform into a buffer, rendering, and repeating, as you would in OpenGL
+//
+// this isn't very efficient and doesn't play well with animation, so not sure how this is typically accomplished with webgpu currently
+//
+// in Vulkan, you'd create a push constant, which lets you do essentially that temporary opengl data thing. WebGPU says its going to support them but afaik not yet
 class Mesh {
-    constructor(ctx, meshLayout, vertexBuffer, indexBuffer, mat) {
-        // create vertex buffer
-        this.vertices = ctx.device.createBuffer({
-            size: vertexBuffer.byteLength,
+    constructor(ctx, meshLayout, positionBuffer, normalBuffer, indexBuffer, materialIndex, mat) {
+        this.materialIndex = materialIndex;
+
+        // create positions buffer
+        this.positions = ctx.device.createBuffer({
+            size: positionBuffer.byteLength,
             usage: GPUBufferUsage.VERTEX,
             mappedAtCreation: true
         });
-        new Uint8Array(this.vertices.getMappedRange()).set(new Uint8Array(vertexBuffer));
-        this.vertices.unmap();
+        new Uint8Array(this.positions.getMappedRange()).set(new Uint8Array(positionBuffer));
+        this.positions.unmap();
+
+        // create normals buffer
+        this.normals = ctx.device.createBuffer({
+            size: normalBuffer.byteLength,
+            usage: GPUBufferUsage.VERTEX,
+            mappedAtCreation: true
+        });
+        new Uint8Array(this.normals.getMappedRange()).set(new Uint8Array(normalBuffer));
+        this.normals.unmap();
 
         // create index buffer
         this.indices = ctx.device.createBuffer({
@@ -155,17 +256,18 @@ class Mesh {
         new Uint8Array(this.indices.getMappedRange()).set(new Uint8Array(indexBuffer));
         this.indices.unmap();
 
-        const float_size = 4; // bytes
-        const mat4_size = float_size * 16;
-
+        // transform
         const matrixBuffer = ctx.device.createBuffer({
-            size: mat4_size,
+            size: MAT4_SIZE * 2,
             usage: GPUBufferUsage.UNIFORM,
             mappedAtCreation: true,
         });
 
-        console.log(mat);
         new Float32Array(matrixBuffer.getMappedRange()).set(mat);
+        const inv_transpose = glMatrix.mat4.create();
+        glMatrix.mat4.invert(inv_transpose, mat);
+        glMatrix.mat4.transpose(inv_transpose, inv_transpose);
+        new Float32Array(matrixBuffer.getMappedRange()).set(inv_transpose, 16);
         matrixBuffer.unmap();
 
         this.to_world = ctx.device.createBindGroup({
@@ -180,29 +282,73 @@ class Mesh {
             ],
         });
 
+        this.indexCount = indexBuffer.byteLength / 2; // u16 indices
+    }
+}
 
-        this.indexCount = indexBuffer.byteLength / 2;
+class Light {
+    constructor(ctx, layout, position, intensity) {
+        const buffer = ctx.device.createBuffer({
+            size: VEC3_SIZE + FLOAT_SIZE,
+            usage: GPUBufferUsage.UNIFORM,
+            mappedAtCreation: true, 
+        });
+
+        new Float32Array(buffer.getMappedRange()).set(position);
+        new Float32Array(buffer.getMappedRange()).set(new Float32Array([intensity]), 3);
+        buffer.unmap();
+
+        this.data = ctx.device.createBindGroup({
+            layout,
+            entries: [
+                {
+                    binding: 0,
+                    resource: {
+                        buffer,
+                    },
+                },
+            ],
+        });
+    }
+}
+
+class Material {
+    constructor(ctx, layout, color, roughness) {
+        const buffer = ctx.device.createBuffer({
+            size: VEC3_SIZE + FLOAT_SIZE,
+            usage: GPUBufferUsage.UNIFORM,
+            mappedAtCreation: true, 
+        });
+
+        new Float32Array(buffer.getMappedRange()).set(color);
+        new Float32Array(buffer.getMappedRange()).set(new Float32Array([roughness]), 3);
+        buffer.unmap();
+
+        this.data = ctx.device.createBindGroup({
+            layout,
+            entries: [
+                {
+                    binding: 0,
+                    resource: {
+                        buffer,
+                    },
+                },
+            ],
+        });
     }
 }
 
 class Camera {
     constructor(ctx, layout, info, mat) {
-        const float_size = 4; // bytes
-        const mat4_size = float_size * 16;
-
         const matrices_buffer = ctx.device.createBuffer({
-            size: mat4_size * 2,
+            size: MAT4_SIZE * 2 + VEC3_SIZE + FLOAT_SIZE, // need to pad
             usage: GPUBufferUsage.UNIFORM,
             mappedAtCreation: true, 
         });
 
         const proj = glMatrix.mat4.create();
-        glMatrix.mat4.perspective(proj, info.yfov, info.aspectRatio, info.znear, info.zfar);
+        glMatrix.mat4.perspective(proj, info.yfov, ctx.aspect, info.znear, info.zfar);
         new Float32Array(matrices_buffer.getMappedRange()).set(proj);
-
-        //let eye = mat.transform_point3(Vec3::new(0.0, 0.0, 0.0));
-        //let target = mat.transform_point3(Vec3::new(0.0, 0.0, -1.0));
-        //let up = mat.transform_vector3(Vec3::new(0.0, 1.0, 0.0));
 
         const eye = glMatrix.vec3.create();
         glMatrix.vec3.transformMat4(eye, eye, mat);
@@ -218,6 +364,7 @@ class Camera {
         const view = glMatrix.mat4.create();
         glMatrix.mat4.lookAt(view, eye, target, up);
         new Float32Array(matrices_buffer.getMappedRange()).set(view, 16);
+        new Float32Array(matrices_buffer.getMappedRange()).set(eye, 32);
 
         matrices_buffer.unmap();
 
@@ -242,19 +389,26 @@ async function createShaderModule(ctx, filePath) {
     return shaderModule;
 }
 
-function createPipeline(ctx, shaderModule, transformLayout) {
-    const layout = ctx.device.createPipelineLayout({ bindGroupLayouts: [transformLayout, transformLayout] });
+function createPipeline(ctx, shaderModule, pipelineLayout) {
     const renderPipeline = ctx.device.createRenderPipeline({
-        layout,
+        layout: pipelineLayout,
         vertex: {
             module: shaderModule,
             entryPoint: "vertex_main",
-            buffers: [{
-                arrayStride: 3 * 4, // in bytes
-                attributes: [
-                    { format: "float32x3", offset: 0, shaderLocation: 0 },
-                ],
-            }]
+            buffers: [
+                {
+                    arrayStride: VEC3_SIZE,
+                    attributes: [
+                        { format: "float32x3", offset: 0, shaderLocation: 0 }, // positions
+                    ],
+                },
+                {
+                    arrayStride: VEC3_SIZE,
+                    attributes: [
+                        { format: "float32x3", offset: 0, shaderLocation: 1 }, // normals
+                    ],
+                },
+            ]
         },
         fragment: {
             module: shaderModule,
@@ -276,12 +430,13 @@ function render(ctx, pipeline, scene) {
 
     const encoder = ctx.device.createCommandEncoder();
 
+    // record GPU instructors
     const renderPass = encoder.beginRenderPass({
         colorAttachments: [{
             view,
             storeOp: "store",
             loadOp: "clear",
-            loadValue: [0.3, 0.3, 0.4, 1]
+            loadValue: [0.6, 0.6, 0.8, 1] // background color
         }],
         depthStencilAttachment: {
             view: ctx.depthTexture.createView(),
@@ -295,44 +450,22 @@ function render(ctx, pipeline, scene) {
     renderPass.setPipeline(pipeline);
 
     renderPass.setBindGroup(0, scene.camera.matrices);
+    renderPass.setBindGroup(2, scene.light.data);
 
     for (const mesh of scene.meshes) {
         renderPass.setBindGroup(1, mesh.to_world);
-        renderPass.setVertexBuffer(0, mesh.vertices);
+        renderPass.setBindGroup(3, scene.materials[mesh.materialIndex].data);
+        renderPass.setVertexBuffer(0, mesh.positions);
+        renderPass.setVertexBuffer(1, mesh.normals);
         renderPass.setIndexBuffer(mesh.indices, "uint16");
         renderPass.drawIndexed(mesh.indexCount);
     }
-    console.log(scene);
 
     renderPass.endPass();
 
+    // submit GPU instructors
     ctx.device.queue.submit([encoder.finish()]);
 }
 
-let ctx;
-try {
-    ctx = await new Context();
-} catch(e) {
-    alert("WebGPU is not supported/enabled in your browser.")
-    throw e;
-}
-console.log(ctx);
-
-const shaderModule = await createShaderModule(ctx, "/shader.wgsl");
-
-const transformLayout = ctx.device.createBindGroupLayout({ entries: [{
-    binding: 0,
-    visibility: GPUShaderStage.VERTEX,
-}]});
-
-const inputElement = document.getElementById("file-input");
-
-const pipeline = createPipeline(ctx, shaderModule, transformLayout);
-
-inputElement.addEventListener("change", async function() {
-    if (this.files.length > 0) {
-        let scene = await new Scene(ctx, transformLayout, this.files[0]);
-        requestAnimationFrame(() => render(ctx, pipeline, scene));
-    }
-}, false);
+await main();
 
--- shader.wgsl
+++ shader.wgsl
@@ -1,6 +1,7 @@
 struct Camera {
     proj: mat4x4<f32>;
     view: mat4x4<f32>;
+    position: vec3<f32>;
 };
 
 [[group(0), binding(0)]]
@@ -8,18 +9,37 @@ var<uniform> camera: Camera;
 
 struct Model {
     to_world: mat4x4<f32>;
+    to_obj_transpose: mat4x4<f32>;
 };
 
 [[group(1), binding(0)]]
 var<uniform> model: Model;
 
+struct Light {
+    position: vec3<f32>;
+    intensity: f32;
+};
+
+[[group(2), binding(0)]]
+var<uniform> light: Light;
+
+struct Material {
+    color: vec3<f32>;
+    roughness: f32;
+};
+
+[[group(3), binding(0)]]
+var<uniform> material: Material;
+
 struct VertexOutput {
     [[builtin(position)]] position: vec4<f32>;
     [[location(0)]] world_position: vec3<f32>;
+    [[location(1)]] world_normal: vec3<f32>;
 };
 
 struct VertexInput {
     [[location(0)]] position: vec3<f32>;
+    [[location(1)]] normal: vec3<f32>;
 };
 
 [[stage(vertex)]]
@@ -27,11 +47,98 @@ fn vertex_main(vert: VertexInput) -> VertexOutput {
     var out: VertexOutput;
     let world_position = model.to_world * vec4<f32>(vert.position, 1.0);
     out.world_position = world_position.xyz;
+    out.world_normal = (model.to_obj_transpose * vec4<f32>(vert.normal, 0.0)).xyz;
     out.position = camera.proj * camera.view * world_position;
     return out;
 }
 
+let PI: f32 = 3.14159265358979323846264;
+
+// The Fresnel reflection factor
+//   i -- incoming direction
+//   m -- microsurface normal
+//   eta -- refractive index
+fn fresnel(i: vec3<f32>, m: vec3<f32>, eta: f32) -> f32 {
+    let c = abs(dot(i, m));
+    let g = sqrt(eta * eta - 1.0 + c * c);
+
+    let gmc = g - c;
+    let gpc = g + c;
+    let nom = c * (g + c) - 1.0;
+    let denom = c * (g - c) + 1.0;
+    return 0.5 * gmc * gmc / gpc / gpc * (1.0 + nom * nom / denom / denom);
+}
+
+// The one-sided Smith shadowing/masking function
+//   v -- in or out vector
+//   m -- microsurface normal
+//   n -- (macro) surface normal
+//   alpha -- surface roughness
+fn G1(v: vec3<f32>, m: vec3<f32>, n: vec3<f32>, alpha: f32) -> f32 {
+    let vm = dot(v, m);
+    let vn = dot(v, n);
+    var result: f32 = 0.0;
+    if (vm * vn > 0.0) {
+        let cosThetaV = dot(n, v);
+        let sinThetaV2 = 1.0 - cosThetaV * cosThetaV;
+        let tanThetaV2 = sinThetaV2 / cosThetaV / cosThetaV;
+        result = 2.0 / (1.0 + sqrt(1.0 + alpha * alpha * tanThetaV2));
+    }
+    return result;
+}
+
+// The GGX slope distribution function
+//   m -- microsurface normal
+//   n -- (macro) surface normal
+//   alpha -- surface roughness
+fn D(m: vec3<f32>, n: vec3<f32>, alpha: f32) -> f32 {
+    let mn = dot(m, n);
+    var result: f32 = 0.0;
+    if (mn > 0.0) {
+        let cosThetaM = mn;
+        let cosThetaM2 = cosThetaM * cosThetaM;
+        let tanThetaM2 = (1.0 - cosThetaM2) / cosThetaM2;
+        let cosThetaM4 =  cosThetaM * cosThetaM * cosThetaM * cosThetaM;
+        let X = (alpha * alpha + tanThetaM2);
+        result = alpha * alpha / (PI * cosThetaM4 * X * X);
+    }
+    return result;
+}
+
+// Evalutate the Microfacet BRDF (GGX variant) for the paramters:
+//   i -- incoming direction (unit vector, pointing away from surface)
+//   o -- outgoing direction (unit vector, pointing away from surface)
+//   n -- outward pointing surface normal vector
+//   eta -- refractive index
+//   alpha -- surface roughness
+// return: scalar BRDF value
+fn isotropicMicrofacet(i: vec3<f32>, o: vec3<f32>, n: vec3<f32>, eta: f32, alpha: f32) -> f32 {
+    let odotn = dot(o, n);
+    let m = normalize(i + o);
+
+    let idotn = dot(i,n);
+    if (idotn <= 0.0 || odotn <= 0.0) {
+        return 0.0;
+    }
+
+    let idotm = dot(i, m);
+    var F: f32 = 0.0;
+    if (idotm > 0.0) {
+        F = fresnel(i,m,eta);
+    }
+    let G = G1(i, m, n, alpha) * G1(o, m, n, alpha);
+    return F * G * D(m, n, alpha) / (4.0 * idotn * odotn);
+}
+
 [[stage(fragment)]]
 fn fragment_main(vert: VertexOutput) -> [[location(0)]] vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+    let to_light = light.position - vert.world_position;
+    let light_direction = normalize(to_light);
+    let view_direction = normalize(camera.position - vert.world_position);
+    let specular = isotropicMicrofacet(light_direction, view_direction, vert.world_normal, 1.5, material.roughness);
+    let brdf = material.color / PI + vec3<f32>(specular);
+    let r2 = dot(to_light, to_light);
+    let k_light = light.intensity * max(dot(vert.world_normal, light_direction), 0.0) / (4.0 * PI * r2);
+    return vec4<f32>(pow(brdf * k_light, vec3<f32>(1.0/2.2)), 1.0);
 }
+
